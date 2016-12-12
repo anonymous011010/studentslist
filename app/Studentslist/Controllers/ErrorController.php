34f87545b92b73bb4aecb0c8d391c579fc09703d
anonymous01 <anonymous01@yandex.com>
2016-12-12 12:29:18 +0200

    fix navbar in 404 page

--- app/Studentslist/Controllers/ErrorController.php
+++ app/Studentslist/Controllers/ErrorController.php
@@ -3,6 +3,7 @@
 namespace Studentslist\Controllers;
 
 use Studentslist\Core\Controller;
+use Studentslist\Core\Auth;
 use Studentslist\Helpers\NavHelper;
 
 /**
@@ -15,10 +16,22 @@ class ErrorController extends Controller {
      * Действие вывода ошибки 404
      */
     public function error404() {
+        
+        $gateway = $this->DbGateway();
+        
+        $token = (isset($_COOKIE['authToken'])) ? \filter_input(\INPUT_COOKIE, 'authToken', \FILTER_SANITIZE_STRING) : '';
+        $auth = new Auth($gateway);
+
+        if ($auth->isRegistred($token)) {
+            $form = 'edit';
+        } else {
+            $form = 'register';
+        }
+
         header('HTTP/1.1 404 Not Found');
         $data['host'] = \APP_URL_ADDR;
         $data['title'] = "Error 404 Not Found";
-        $navHelper = new NavHelper($data['host']);
+        $navHelper = new NavHelper($data['host'], $form);
         self::renderErrorView('404', $navHelper, $data);
         die();
     }
